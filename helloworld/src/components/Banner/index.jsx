69d6461a62982abc5058170ebbe656be58b97881
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-04-11 10:05:11 +0200

    index css
    reset style and root

---
+++ helloworld/src/components/Banner/index.jsx
@@ -0,0 +1,7 @@
+import styles from "./Banner.module.css";
+import colorfulCircle from "assets/circulo_colorido.png";
+import myPicture from "assets/minha_foto.png";
+
+export default function Banner() {
+  return;
+}
