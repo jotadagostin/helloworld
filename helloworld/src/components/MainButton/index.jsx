f3f959ce50302de7d50eeddf31922e7942a0393b
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-04-12 22:38:36 +0200

    mainbutton

---
+++ helloworld/src/components/MainButton/index.jsx
@@ -0,0 +1,14 @@
+import styles from "./MainButton.module.css";
+
+
+export default function MainButton({ children, size}) {
+    return (
+        <button className={`
+            ${styles.mainButton}
+            ${styles[size]}
+        `}>
+            {children}
+        </button>
+    )
+
+}
