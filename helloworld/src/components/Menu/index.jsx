8d8da4f60bedc7da15a03de0ce4f09eb16caf1f5
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-04-12 23:22:28 +0200

    menulink

---
+++ helloworld/src/components/Menu/index.jsx
@@ -0,0 +1,12 @@
+import Menulink from  '../MenuLink'
+import styles from './Menu.module.css'
+
+export default function Menu() {
+    return (
+        <header>
+            <nav className={styles.navegation}>
+                
+            </nav>
+        </header>
+    )
+}
