13aa43479519b3f968f0ee3239d21466e194702b
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-04-12 22:30:59 +0200

    Update index.jsx
    Banner

--- helloworld/src/components/Banner/index.jsx
+++ helloworld/src/components/Banner/index.jsx
@@ -5,10 +5,28 @@ import myPicture from "assets/minha_foto.png";
 export default function Banner() {
   return (
     <div className={styles.banner}>
-      <div>
-        
+      <div className={styles.apresentacao}>
+        <h1 className={styles.title}>Hello, World!</h1>
+        <p className={styles.paragraph}>
+          Welcome to my personal space! I'm Antônio Evaldo, Front-end instructor
+          at Alura. Here I share a lot of knowledge, I hope you learn something
+          new :)
+        </p>
       </div>
 
+      <div className={styles.images}>
+        <img
+          src={colorfulCircle}
+          alt=""
+          className={styles.colorfulCircle}
+          aria-hidden={true}
+        />
+
+        <img 
+          src={myPicture} 
+          alt="" 
+          className={styles.myPicture} />
+      </div>
     </div>
-  )
+  );
 }
