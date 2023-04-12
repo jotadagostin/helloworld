c195880deb7825ea04c95bb64d03b387194c0cf3
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-04-12 23:25:07 +0200

    Update index.jsx
    menu

--- helloworld/src/components/Menu/index.jsx
+++ helloworld/src/components/Menu/index.jsx
@@ -1,12 +1,13 @@
-import Menulink from  '../MenuLink'
-import styles from './Menu.module.css'
+import MenuLink from "../MenuLink";
+import styles from "./Menu.module.css";
 
 export default function Menu() {
-    return (
-        <header>
-            <nav className={styles.navegation}>
-                
-            </nav>
-        </header>
-    )
+  return (
+    <header>
+      <nav className={styles.navegation}>
+        <MenuLink to="/">Start</MenuLink>
+        <MenuLink to="/aboutme">About Me</MenuLink>
+      </nav>
+    </header>
+  );
 }
