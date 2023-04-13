460a770c54d37991c7ee48c36c16c9620cd108e3
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-04-13 09:42:46 +0200

    update components
    postcard

--- helloworld/src/components/PostCard/index.jsx
+++ helloworld/src/components/PostCard/index.jsx
@@ -2,14 +2,19 @@ import MainButton from "components/MainButton";
 import { Link } from "react-router-dom";
 import styles from "./Post.module.css";
 
+export default function PostCard({ post }) {
+  return (
+    <Link to={`/post/${post.id}`}>
+      <div className={styles.post}>
+        <img
+          className={styles.cover}
+          src={`/assets/posts/${post.id}/capa.png`}
+          alt="cover picture"
+        />
+        <h2 className={styles.title}>{post.title}</h2>
 
-export default function PostCard( { post }) {
-    return (
-        <Link to={`/post/${post.id}`}>
-            <div className={}>
-
-            </div>
-        
-        </Link>
-    )
+        <MainButton>Read</MainButton>
+      </div>
+    </Link>
+  );
 }
