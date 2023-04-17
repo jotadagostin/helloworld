670532d5998e447353bb6d6ae9fd431406338398
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-04-17 09:55:37 +0200

    stargint route.js

---
+++ helloworld/src/routes.js
@@ -0,0 +1,9 @@
+import StandardPage from "components/StandardPage";
+import Footer from "components/Footer";
+import ScrollToTop from "components/ScrollToTop";
+import NotFound from "pages/NotFound";
+import Post from "pages/Post";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Menu from "components/Menu";
+import Home from "./pages/Home";
+import AboutMe from "./pages/AboutMe";
