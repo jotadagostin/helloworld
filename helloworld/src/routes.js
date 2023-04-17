58c2a83bcf34baf7f8f0b933ac689282afbf6960
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-04-17 10:13:03 +0200

    update index
    route.js

--- helloworld/src/routes.js
+++ helloworld/src/routes.js
@@ -7,3 +7,24 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Menu from "components/Menu";
 import Home from "./pages/Home";
 import AboutMe from "./pages/AboutMe";
+
+function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <ScrollToTop />
+      <Menu />
+
+      <Routes>
+        <Route path="/" element={<StandardPage />}>
+          <Route index element={<Home />} />
+          <Route path="aboutme" element={<AboutMe />} />
+        </Route>
+
+        <Route path="posts/:id/*" element={<Post />}/>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+
+      <Footer />
+    </BrowserRouter>
+  );
+}
